d1e31dfba0f9652869ae7835e27ae66b7740b744
Ruzutsas <Ruzutsas@users.noreply.github.com>
2022-04-29 18:47:16 +0200

    For loop excercise
    What is the output of the loops?

---
+++ For_excercise.m
@@ -0,0 +1,20 @@
+A = [1 9 8 4 6 2 5];
+for i = 1:length(A)
+    B = A(i);
+end
+
+for i = 1:length(A)
+    C(i) = A(i);
+end
+
+for i = 1:3
+    D = 0;
+    D = D + A(i);
+end
+
+for i = 1:3
+    for ii = 1:length(A)
+        F(i) = A(ii);
+    end
+    A(i) = A(i) + 1;
+end
